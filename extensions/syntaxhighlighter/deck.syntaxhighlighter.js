4c16a20fe3ccc9c123d75276f5e17f79fff597e8
m8pple <dt10@imperial.ac.uk>
2015-02-24 17:35:20 +0000

    Pushy

--- extensions/syntaxhighlighter/deck.syntaxhighlighter.js
+++ extensions/syntaxhighlighter/deck.syntaxhighlighter.js
@@ -11,28 +11,68 @@ currently has a fixed absolute prefix. */
     /*'<link href="http://alexgorbatchev.com/pub/sh/current/styles/shThemeDefault.css" rel="stylesheet" type="text/css" />'*/
     '<link href="/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shThemeDefault.css" rel="stylesheet" type="text/css" />'
     ).append(
-    /* DT10 : Stop scroll-bars appearing in chrome:
-        http://yaseminavcular.blogspot.co.uk/2011/04/remove-unnecessary-scrollbar-from.html
-    */
+    // DT10 : Stop scroll-bars appearing in chrome:
+    //    http://yaseminavcular.blogspot.co.uk/2011/04/remove-unnecessary-scrollbar-from.html
     '<style type="text/css">.syntaxhighlighter { overflow-y: hidden !important; }</style>'
     );
     function setupSyntaxHighlighterAutoloads() {
         console.log("calling SyntaxHighlighter");
-        SyntaxHighlighter.autoloader(
-              'bash shell             /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushBash.js',
+        /*SyntaxHighlighter.autoloader(
+              ['js','jscript','javascript','/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushJScript.js']
+                ,
               'cpp c                  /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushCpp.js',
               'java                   /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushJava.js',
-              'js jscript javascript  /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushJScript.js',
               'text plain             /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushPlain.js',
               'py python              /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushPython.js',
-              'xml xhtml xslt html    /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushXml.js'
-       );
+              'xml xhtml xslt html    /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushXml.js',
+              'matlab                 /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushMatlab.js',
+        'bash shell             /resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushBash.js'
+       );*/
        SyntaxHighlighter.defaults['toolbar'] = false;
+       SyntaxHighlighter.defaults['gutter'] = false;
        SyntaxHighlighter.defaults['tab-size'] = 2;
        SyntaxHighlighter.all();
     }
-    $.getScript("/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shCore.js",
+    
+    /*require(['shCore'],function(shCore){
+        require(['shBrushJScript','shBrushCpp','shBrushJava','shBrushPlain','shBrushPython','shBrushXml','shBrushMatlab','shBrushBash'],function(){
+            setupSyntaxHighlighterAutoloads(shCore);
+        });
+    });*/
+    
+    /*$.getScript("/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shCore.js",
         function () {
             $.getScript("/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shAutoloader.js",setupSyntaxHighlighterAutoloads);
-    });
+    });*/
+    
+    var toLoad=[
+        '/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushJScript.js',
+        '/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushCpp.js',
+        '/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushJava.js',
+        '/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushPlain.js',
+        '/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushPython.js',
+        '/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushXml.js',
+        '/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushMatlab.js',
+        '/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shBrushBash.js'
+    ];
+    
+    
+    function loadAll()
+    {
+        if(toLoad.length==0){
+           SyntaxHighlighter.defaults['toolbar'] = false;
+           SyntaxHighlighter.defaults['gutter'] = false;
+           SyntaxHighlighter.defaults['tab-size'] = 2;
+           console.log("SyntaxHighlighter.all()");
+           SyntaxHighlighter.all();
+        }else{
+            console.log(toLoad);
+            var todo=toLoad.pop();
+            $.getScript(todo, loadAll);
+        }
+    }
+    
+    $.getScript('/resources/deck.js/extensions/syntaxhighlighter/SyntaxHighlighter/shCore.js',
+        loadAll
+    );
 })(jQuery);
